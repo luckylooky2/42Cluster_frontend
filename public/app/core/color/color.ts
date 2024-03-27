2d4c92037e92654add5f3f8a31609dae16648618
luckylooky2 <luckylooky2@naver.com>
2024-03-27 11:32:19 +0900

    Feat: Color object from github

---
+++ public/app/core/color/color.ts
@@ -0,0 +1,50 @@
+export const customButtonColor = {
+  dark: {
+    default: {
+      color: '#c9d1d9',
+      basic: {
+        background: '#21262d',
+        borderColor: 'rgba(240,246,252,0.1)',
+      },
+      hover: {
+        background: '#30363d',
+        borderColor: '#8b949e',
+      },
+    },
+    green: {
+      color: '#c9d1d9',
+      basic: {
+        background: '#238636',
+        borderColor: 'rgba(240,246,252,0.1)',
+      },
+      hover: {
+        background: '#2ea043',
+        borderColor: 'rgba(240,246,252,0.1)',
+      },
+    },
+  },
+  light: {
+    default: {
+      color: '#24292f',
+      basic: {
+        background: '#f6f8fa',
+        borderColor: '#cccccc',
+      },
+      hover: {
+        background: '#f3f4f6',
+        borderColor: '#cccccc',
+      },
+    },
+    green: {
+      color: '#ffffff',
+      basic: {
+        borderColor: 'rgba(31,35,40,0.15)',
+        background: '#1f883d',
+      },
+      hover: {
+        background: '#1a7f37',
+        borderColor: 'rgba(31,35,40,0.15)',
+      },
+    },
+  },
+};
