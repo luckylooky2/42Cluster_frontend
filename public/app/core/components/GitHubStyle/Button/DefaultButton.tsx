15f9446d0d7b1610493f50e777fc3c94b0271f00
luckylooky2 <luckylooky2@naver.com>
2024-05-03 19:06:04 +0900

    Fix: props rename, integrate using cx #31

--- public/app/core/components/GitHubStyle/Button/DefaultButton.tsx
+++ public/app/core/components/GitHubStyle/Button/DefaultButton.tsx
@@ -1,21 +1,26 @@
-import { css, CSSInterpolation } from '@emotion/css';
+import { cx } from '@emotion/css';
 import React from 'react';
 
 import { useStyles2 } from '@grafana/ui';
 
-import { GitHubButtonStyles } from '../../../../../style/GitHubButton';
+import { GitHubButtonStyles } from '../../../../../style/GitHubButtonStyles';
 
 interface Props {
   text: string;
-  style?: CSSInterpolation;
+  onClick: () => void;
+  className?: string;
   type?: string;
 }
 
-const DefaultButton = ({ text, style, type = 'default' }: Props) => {
+const DefaultButton = ({ text, onClick, className, type = 'default' }: Props) => {
   const gitHubButtonStyles = useStyles2(GitHubButtonStyles);
   const buttonCSS = type === 'default' ? gitHubButtonStyles.basicButton : gitHubButtonStyles.greenButton;
 
-  return <button className={`${gitHubButtonStyles.button} ${buttonCSS} ${css(style)}`}>{text}</button>;
+  return (
+    <button className={cx(gitHubButtonStyles.button, buttonCSS, className)} onClick={onClick}>
+      {text}
+    </button>
+  );
 };
 
 export default DefaultButton;
