af1646af0505ee6a7d3786c79598a57698d43e8c
luckylooky2 <luckylooky2@naver.com>
2024-05-04 23:23:44 +0900

    Design: dark mode toggle button CSS #33

--- public/app/core/components/AppChrome/TopBar/DarkModeToggle.tsx
+++ public/app/core/components/AppChrome/TopBar/DarkModeToggle.tsx
@@ -24,7 +24,9 @@ const DarkModeToggle = () => {
         }}
       />
       <label htmlFor="switch" className={styles.switchLabel}>
+        {isDark && <img className={styles.dark} src="public/img/42Cluster/moon.png" alt="dark" />}
         <span className={styles.button}>{}</span>
+        {!isDark && <img className={styles.light} src="public/img/42Cluster/sun.png" alt="light" />}
       </label>
     </div>
   );
@@ -36,34 +38,26 @@ const getStyles = (theme: GrafanaTheme2) => ({
     position: 'relative',
     cursor: 'pointer',
     display: 'inline-block',
-    width: '58px',
-    height: '28px',
+    width: '60px',
+    height: '30px',
     background: '#fff',
-    border: '2px solid #daa',
+    border: '1px solid',
+    borderColor: theme.colors.border.strong,
     borderRadius: '20px',
     transition: '0.2s',
-
-    '&:hover': {
-      background: '#efefef',
-    },
   }),
   switch: css({
     position: 'absolute',
     appearance: 'none',
 
     '&:checked + label': {
-      background: '#c44',
-      border: '2px solid #c44',
-    },
-
-    '&:checked + label:hover': {
-      background: '#e55',
+      background: '#39ace7',
+      border: '1px solid #39ace7',
     },
 
     '&:checked + label span': {
       left: '33px',
       background: '#fff',
-      boxShadow: '1px 2px 3px #00000020',
     },
   }),
   button: css({
@@ -71,11 +65,31 @@ const getStyles = (theme: GrafanaTheme2) => ({
     top: '3px',
     left: '5px',
     display: 'inline-block',
-    width: '19px',
-    height: '19px',
+    width: '22px',
+    height: '22px',
     borderRadius: '20px',
-    background: '#daa',
+    background: theme.colors.border.strong,
     transition: '0.2s',
+  }),
+  light: css({
+    position: 'absolute',
+    top: '6px',
+    left: '35px',
+    display: 'inline-block',
+    filter: 'invert(20%)',
+    width: '15px',
+    height: '15px',
+    zIndex: '10',
+  }),
+  dark: css({
+    position: 'absolute',
+    top: '7px',
+    left: '10px',
+    display: 'inline-block',
+    filter: 'invert(100%)',
+    width: '15px',
+    height: '15px',
+    zIndex: '10',
   }),
 });
 
