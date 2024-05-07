ab14d0b121a6470d7d18a7b93229bdf21aca4b94
luckylooky2 <luckylooky2@naver.com>
2024-05-07 18:17:14 +0900

    Feat: replace Select comp. with Menu comp. #34

--- public/app/core/components/AppChrome/OrganizationSwitcher/OrganizationSelect.tsx
+++ public/app/core/components/AppChrome/OrganizationSwitcher/OrganizationSelect.tsx
@@ -1,8 +1,9 @@
-import { css } from '@emotion/css';
+import { css, cx } from '@emotion/css';
 import React, { useState } from 'react';
+import { GitHubHoverStyles } from 'style/GitHubHoverStyles';
 
 import { SelectableValue, GrafanaTheme2 } from '@grafana/data';
-import { Icon, Select, useStyles2 } from '@grafana/ui';
+import { Menu, Dropdown, useStyles2, ToolbarButton } from '@grafana/ui';
 import { contextSrv } from 'app/core/services/context_srv';
 import { UserOrg } from 'app/types';
 
@@ -11,6 +12,7 @@ import { OrganizationBaseProps } from './types';
 export function OrganizationSelect({ orgs, onSelectChange }: OrganizationBaseProps) {
   const styles = useStyles2(getStyles);
   const { orgName: name, orgId, orgRole: role } = contextSrv.user;
+  const [isOpen, setIsOpen] = useState(false);
   const [value, setValue] = useState<SelectableValue<UserOrg>>(() => ({
     label: name,
     value: { role, orgId, name },
@@ -20,35 +22,50 @@ export function OrganizationSelect({ orgs, onSelectChange }: OrganizationBasePro
     setValue(option);
     onSelectChange(option);
   };
+  const gitHubHoverStyles = useStyles2(GitHubHoverStyles);
+
+  const createActions = orgs.map((org) => ({
+    label: org.name,
+    description: org.role,
+    value: org,
+  }));
+
+  const MenuActions = () => {
+    return (
+      <Menu>
+        {createActions.map((createAction, index) => (
+          <Menu.Item
+            key={index}
+            label={createAction.label}
+            checkType={true}
+            isChecked={value.label === createAction.label}
+            onClick={() => {
+              onChange(createAction);
+              return onSelectChange;
+            }}
+          />
+        ))}
+      </Menu>
+    );
+  };
 
   return (
-    <Select<UserOrg>
-      aria-label="Change organization"
-      width={'auto'}
-      value={value}
-      prefix={<Icon className="prefix-icon" name="building" />}
-      className={styles.select}
-      options={orgs.map((org) => ({
-        label: org.name,
-        description: org.role,
-        value: org,
-      }))}
-      onChange={onChange}
-    />
+    <Dropdown overlay={MenuActions} placement="bottom-start" onVisibleChange={setIsOpen}>
+      <div>
+        <ToolbarButton isOpen={isOpen} className={cx(styles.button, gitHubHoverStyles.default)} aria-label="New">
+          <b>{value.label}</b>
+        </ToolbarButton>
+      </div>
+    </Dropdown>
   );
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
-  select: css({
-    border: 'none',
-    background: 'none',
-    color: theme.colors.text.secondary,
-    '&:hover': {
-      color: theme.colors.text.primary,
-
-      '& .prefix-icon': css({
-        color: theme.colors.text.primary,
-      }),
-    },
+  button: css({
+    width: '100%',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    padding: theme.spacing(0.5, 1, 0.5, 1),
   }),
 });
