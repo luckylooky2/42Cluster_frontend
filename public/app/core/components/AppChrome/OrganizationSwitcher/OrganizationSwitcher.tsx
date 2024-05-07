8d52d4244a47b738d1f2be81c49e83799fc3a974
luckylooky2 <luckylooky2@naver.com>
2024-05-07 18:15:47 +0900

    Feat: always remain OrganizationSelect dropdown #34

--- public/app/core/components/AppChrome/OrganizationSwitcher/OrganizationSwitcher.tsx
+++ public/app/core/components/AppChrome/OrganizationSwitcher/OrganizationSwitcher.tsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import { css } from '@emotion/css';
+import React, { useEffect } from 'react';
 
-import { SelectableValue } from '@grafana/data';
+import { SelectableValue, GrafanaTheme2 } from '@grafana/data';
 import { locationService } from '@grafana/runtime';
-import { useTheme2 } from '@grafana/ui';
-import { useMediaQueryChange } from 'app/core/hooks/useMediaQueryChange';
+import { useStyles2 } from '@grafana/ui';
+// import { useMediaQueryChange } from 'app/core/hooks/useMediaQueryChange';
 import { contextSrv } from 'app/core/services/context_srv';
 import { getUserOrganizations, setUserOrganization } from 'app/features/org/state/actions';
 import { useDispatch, useSelector, UserOrg } from 'app/types';
 
-import { OrganizationPicker } from './OrganizationPicker';
+// import { OrganizationPicker } from './OrganizationPicker';
 import { OrganizationSelect } from './OrganizationSelect';
 
 export function OrganizationSwitcher() {
-  const theme = useTheme2();
+  const styles = useStyles2(getStyles);
+  // const theme = useTheme2();
   const dispatch = useDispatch();
   const orgs = useSelector((state) => state.organization.userOrgs);
   const onSelectChange = (option: SelectableValue<UserOrg>) => {
@@ -29,22 +31,34 @@ export function OrganizationSwitcher() {
     }
   }, [dispatch]);
 
-  const breakpoint = theme.breakpoints.values.sm;
+  // const breakpoint = theme.breakpoints.values.sm;
 
-  const [isSmallScreen, setIsSmallScreen] = useState(!window.matchMedia(`(min-width: ${breakpoint}px)`).matches);
+  // const [isSmallScreen, setIsSmallScreen] = useState(!window.matchMedia(`(min-width: ${breakpoint}px)`).matches);
 
-  useMediaQueryChange({
-    breakpoint,
-    onChange: (e) => {
-      setIsSmallScreen(!e.matches);
-    },
-  });
+  // useMediaQueryChange({
+  //   breakpoint,
+  //   onChange: (e) => {
+  //     setIsSmallScreen(!e.matches);
+  //   },
+  // });
 
   if (orgs?.length <= 1) {
     return null;
   }
 
-  const Switcher = isSmallScreen ? OrganizationPicker : OrganizationSelect;
+  // const Switcher = isSmallScreen ? OrganizationPicker : OrganizationSelect;
 
-  return <Switcher orgs={orgs} onSelectChange={onSelectChange} />;
+  return (
+    <>
+      <div className={styles.slash}>/</div>
+      <OrganizationSelect orgs={orgs} onSelectChange={onSelectChange} />
+    </>
+  );
 }
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  slash: css({
+    padding: theme.spacing(0.5),
+    color: '#777',
+  }),
+});
