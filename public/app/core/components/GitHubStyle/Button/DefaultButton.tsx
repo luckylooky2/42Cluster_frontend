32b61d6e562f3bc42798e1fb9c721d6fe726cf6c
luckylooky2 <luckylooky2@naver.com>
2024-05-24 18:14:46 +0900

    Feat: DefaultButton has children props not text props #37

--- public/app/core/components/GitHubStyle/Button/DefaultButton.tsx
+++ public/app/core/components/GitHubStyle/Button/DefaultButton.tsx
@@ -6,19 +6,19 @@ import { useStyles2 } from '@grafana/ui';
 import { GitHubButtonStyles } from '../../../../../style/GitHubButtonStyles';
 
 interface Props {
-  text: string;
+  children: React.ReactNode;
   onClick: () => void;
   className?: string;
   type?: string;
 }
 
-const DefaultButton = ({ text, onClick, className, type = 'default' }: Props) => {
+const DefaultButton = ({ children, onClick, className, type = 'default' }: Props) => {
   const gitHubButtonStyles = useStyles2(GitHubButtonStyles);
   const buttonCSS = type === 'default' ? gitHubButtonStyles.basicButton : gitHubButtonStyles.greenButton;
 
   return (
     <button className={cx(gitHubButtonStyles.button, buttonCSS, className)} onClick={onClick}>
-      {text}
+      {children}
     </button>
   );
 };
