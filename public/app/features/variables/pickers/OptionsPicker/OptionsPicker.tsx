d7603842a7aeb51dd7ad74a60b858b0ebce36bb4
luckylooky2 <luckylooky2@naver.com>
2024-06-01 11:09:29 +0900

    Feat: apply changes option-multi-select #39

--- public/app/features/variables/pickers/OptionsPicker/OptionsPicker.tsx
+++ public/app/features/variables/pickers/OptionsPicker/OptionsPicker.tsx
@@ -111,16 +111,23 @@ export const optionPickerFactory = <Model extends VariableWithOptions | Variable
     };
 
     render() {
-      const { variable } = this.props;
-
-      // const { variable, picker } = this.props;
+      const { variable, picker } = this.props;
       // const showOptions = picker.id === variable.id; // true: dropdown 열림(picker), false: dropdown 닫힘(variable)
 
       return (
-        <div className="variable-link-wrapper">
-          <OptionDropdown options={variable.options} />
-          {/* {showOptions ? this.renderOptions(picker) : this.renderLink(variable)} */}
-        </div>
+        <>
+          <div className="variable-link-wrapper">
+            <ClickOutsideWrapper onClick={this.onHideOptions}>
+              <OptionDropdown
+                variable={variable}
+                picker={picker}
+                onToggle={this.onToggleOption}
+                showOptions={this.onShowOptions}
+              />
+            </ClickOutsideWrapper>
+            {/* {showOptions ? this.renderOptions(picker) : this.renderLink(variable)} */}
+          </div>
+        </>
       );
     }
 
