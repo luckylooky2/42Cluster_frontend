5d02dc1fd819e4878f75f62e9c9f6cefab6049ee
luckylooky2 <luckylooky2@naver.com>
2024-07-04 18:10:19 +0900

    Feat: remove menu list and change button to expand #49

--- packages/grafana-ui/src/components/PanelChrome/PanelMenu.tsx
+++ packages/grafana-ui/src/components/PanelChrome/PanelMenu.tsx
@@ -1,9 +1,11 @@
 import { cx } from '@emotion/css';
-import React, { ReactElement, useCallback } from 'react';
+import React, { ReactElement, useState } from 'react';
 
+import { PanelModel } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
 
-import { Dropdown } from '../Dropdown/Dropdown';
+// import { Dropdown } from '../Dropdown/Dropdown';
+import { UrlModule } from '../../../../../public/app/features/dashboard/utils/urlModule';
 import { ToolbarButton } from '../ToolbarButton';
 import { TooltipPlacement } from '../Tooltip';
 
@@ -11,6 +13,7 @@ interface PanelMenuProps {
   menu: ReactElement | (() => ReactElement);
   menuButtonClass?: string;
   dragClassCancel?: string;
+  panel?: PanelModel;
   title?: string;
   placement?: TooltipPlacement;
   offset?: [number, number];
@@ -20,6 +23,7 @@ interface PanelMenuProps {
 export function PanelMenu({
   menu,
   title,
+  panel,
   placement = 'bottom',
   offset,
   dragClassCancel,
@@ -27,27 +31,39 @@ export function PanelMenu({
   onOpenMenu,
 }: PanelMenuProps) {
   const testId = title ? selectors.components.Panels.Panel.menu(title) : `panel-menu-button`;
+  const [isExpaned, setIsExpanded] = useState(window.location.search.includes('viewPanel'));
 
-  const handleVisibility = useCallback(
-    (show: boolean) => {
-      if (show && onOpenMenu) {
-        onOpenMenu();
-      }
-    },
-    [onOpenMenu]
-  );
+  // const handleVisibility = useCallback(
+  //   (show: boolean) => {
+  //     if (show && onOpenMenu) {
+  //       onOpenMenu();
+  //     }
+  //   },
+  //   [onOpenMenu]
+  // );
+
+  const expandPanel = () => {
+    new UrlModule().addParam('viewPanel', panel?.id).navigate();
+    setIsExpanded(true);
+  };
+
+  const shrinkPanel = () => {
+    new UrlModule().deleteParam('viewPanel').navigate();
+    setIsExpanded(false);
+  };
 
   return (
-    <Dropdown overlay={menu} placement={placement} offset={offset} onVisibleChange={handleVisibility}>
-      <ToolbarButton
-        aria-label={`Menu for panel with ${title ? `title ${title}` : 'no title'}`}
-        title="Menu"
-        icon="ellipsis-v"
-        iconSize="md"
-        narrow
-        data-testid={testId}
-        className={cx(menuButtonClass, dragClassCancel)}
-      />
-    </Dropdown>
+    // <Dropdown overlay={menu} placement={placement} offset={offset} onVisibleChange={handleVisibility}>
+    <ToolbarButton
+      aria-label={`Menu for panel with ${title ? `title ${title}` : 'no title'}`}
+      title={isExpaned ? 'shrink' : 'expand'}
+      icon={isExpaned ? 'minus' : 'plus'}
+      iconSize="md"
+      narrow
+      data-testid={testId}
+      onClick={isExpaned ? shrinkPanel : expandPanel}
+      className={cx(menuButtonClass, dragClassCancel)}
+    />
+    // </Dropdown>
   );
 }
