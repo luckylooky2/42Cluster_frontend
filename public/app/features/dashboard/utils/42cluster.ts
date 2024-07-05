35713ca754cd3476a652793523a9b2b816c333b5
luckylooky2 <luckylooky2@naver.com>
2024-07-05 15:41:35 +0900

    Refactor: apply getParams method #50

--- public/app/features/dashboard/utils/42cluster.ts
+++ public/app/features/dashboard/utils/42cluster.ts
@@ -1,6 +1,4 @@
-import { getVariablesState } from 'app/features/variables/state/selectors';
-
-import { VariableOption } from '../../../../../packages/grafana-data/src';
+import { UrlModule } from './urlModule';
 
 export const getDashboardUidFromUrl = function () {
   const [DEV, PROD] = [2, 3];
@@ -28,66 +26,31 @@ export const determineUrl = function (index: number) {
 // - 불가능 : getVariablesState를 호출해도 해당 대시보드의 variable을 받아올 수 없음(현재 대시보드만 가능한 것으로 관측)
 // 2) includeAll = true로 만들고, 내부적으로 selectedValues에 모두 체크되도록 실행하기
 
-// 사용되지 않음
-export const variableQueryString = (uid: string) => {
-  const result = getVariablesState(uid);
-  const variable = result.variables.namespace;
-  const prefix = '?';
-
-  if (variable === undefined || !variable.multi) {
-    return '';
-  }
-
-  return prefix + variable.options.map((v: VariableOption) => `var-${variable.id}=${v.value}`).join('&');
-};
-
-export const selectedValuesQueryString = (selectedValues: VariableOption[], variableId: string) => {
-  const prefix = '?';
-
-  if (selectedValues === undefined || selectedValues.length === 0) {
-    return '';
-  }
-
-  return prefix + selectedValues.map((v: VariableOption) => `var-${variableId}=${v.value}`).join('&');
-};
-
 export const isTimeRangeChanged = () => {
-  const qs = window.location.search;
-  if (qs) {
-    const splitted = qs
-      .slice(1)
-      .split('&')
-      .map((v) => v.split('='));
-    let [isFromChanged, isToChanged] = [true, true];
-    const keys = splitted.map((v) => v[0]);
+  const params = new UrlModule().getParams();
+  if (params) {
+    let [isFromChanged, isToChanged] = [false, false];
 
-    if (!keys.includes('from') && !keys.includes('to')) {
-      return false;
-    }
-
-    for (const [key, value] of splitted) {
-      if (key === 'from' && value === 'now-1h') {
-        isFromChanged = false;
+    // 변수화 필요
+    for (const [key, value] of params) {
+      if (key === 'from' && value !== 'now-1h') {
+        isFromChanged = true;
       }
-      if (key === 'to' && value === 'now') {
-        isToChanged = false;
+      if (key === 'to' && value !== 'now') {
+        isToChanged = true;
       }
     }
 
     return isFromChanged || isToChanged ? true : false;
   }
 
-  return true;
+  return false;
 };
 
 export const isTemplateVariableFiltered = () => {
-  const qs = window.location.search;
-  if (qs) {
-    const splitted = qs
-      .slice(1)
-      .split('&')
-      .map((v) => v.split('='));
-    const keys = splitted.map((v) => v[0]);
+  const params = new UrlModule().getParams();
+  if (params) {
+    const keys = params.map((v) => v[0]);
     if (!keys.includes('var-Filters')) {
       return false;
     }
