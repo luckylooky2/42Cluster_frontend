31fd599a7c2a172bd9a526656a0ec23b5cc0c55f
luckylooky2 <luckylooky2@naver.com>
2024-07-05 15:53:05 +0900

    Test: utils test codes

---
+++ public/test/42cluster/utils.test.ts
@@ -0,0 +1,78 @@
+import { isTimeRangeChanged, isTemplateVariableFiltered } from 'app/features/dashboard/utils/42cluster';
+
+describe('Time Range Changed', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'location', {
+      writable: true,
+      value: {
+        pathname: '/d/unique-dashboard-id/',
+        search: '',
+      },
+    });
+  });
+
+  it('default', () => {
+    window.location.search = '?orgId=1&viewPanel=1&from=now-1h&to=now';
+    expect(isTimeRangeChanged()).toBe(false);
+  });
+
+  it('no time variable', () => {
+    window.location.search = '?orgId=1&viewPanel=1';
+
+    expect(isTimeRangeChanged()).toBe(false);
+  });
+
+  it('no variable', () => {
+    expect(isTimeRangeChanged()).toBe(false);
+  });
+
+  it('from variable only', () => {
+    window.location.search = '?from=now-1h';
+    expect(isTimeRangeChanged()).toBe(false);
+  });
+
+  it('to variable only', () => {
+    window.location.search = '?to=now';
+    expect(isTimeRangeChanged()).toBe(false);
+  });
+
+  it('from variable only and changed', () => {
+    window.location.search = '?from=now-2h';
+    expect(isTimeRangeChanged()).toBe(true);
+  });
+
+  it('to variable only and changed', () => {
+    window.location.search = '?to=now-1h';
+    expect(isTimeRangeChanged()).toBe(true);
+  });
+
+  it('changed from variable', () => {
+    window.location.search = '?orgId=1&viewPanel=1&from=now-2h&to=now';
+    expect(isTimeRangeChanged()).toBe(true);
+  });
+});
+
+describe('Template Variable Filtered', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'location', {
+      writable: true,
+      value: {
+        pathname: '/d/unique-dashboard-id/',
+        search: '',
+      },
+    });
+  });
+
+  it('not filtered', () => {
+    window.location.search = '?orgId=1&viewPanel=1&from=now-1h&to=now';
+
+    expect(isTemplateVariableFiltered()).toBe(false);
+  });
+
+  it('filtered', () => {
+    window.location.search =
+      '?orgId=1&var-Filters=namespace%23%70%23mogle&var-Filters=namespace%23%70%23monitoring&var-Filters=node%23%70%23node-1&from=now-2h&to=now&kiosk';
+
+    expect(isTemplateVariableFiltered()).toBe(true);
+  });
+});
